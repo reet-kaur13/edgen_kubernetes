4060359d7ea3ba084fe26aa97d2901b11dcd02ad
SamStrike0809 <1905359@kiit.ac.in>
2023-04-11 20:30:28 +0530

    found old code

--- Webgen/edgen_cli.py
+++ Webgen/edgen_cli.py
@@ -8,21 +8,18 @@ import random
 import os
 import pandas as pd
 from urllib import response
-from flask import jsonify
-from flask_cors import CORS
 import json
-from werkzeug.utils import secure_filename
 import sys
 import os
 
 
 fake = Faker('en-US')
 
-hostname = ""
-database = ""
-username = ""
-pwd = ""
-port_id = 
+hostname = "www.americaniche.com"
+database = "postgres"
+username = "postgres"
+pwd = "password"
+port_id = 33306
 indexes = 1
 conn = psycopg2.connect(host=hostname, database=database,
                         user=username, password=pwd, port=port_id)
@@ -33,7 +30,7 @@ def input_customers():
     cur = conn.cursor()
     cur.execute("ROLLBACK")
 
-    cur.execute("select count(*) from dev.advance_jaffle_shop.customers")
+    cur.execute("select count(*) from postgres.advance_jaffle_shop.customers")
     length1 = cur.fetchone()
     print(length1)
     index1 = 1 if length1[0] < 1 else length1[0] + 1
@@ -46,29 +43,31 @@ def input_customers():
     name1 = fake.name()
     customer_data['f_name'] = name1.split()[0]
     customer_data['l_name'] = name1.split()[1]
-
-    try :
-        cur.execute("insert into dev.advance_jaffle_shop.customers values('{}','{}','{}')".format(customer_data['cid'],customer_data['f_name'],customer_data['l_name']))
+    try:
+        cur.execute("insert into postgres.advance_jaffle_shop.customers values('{}','{}','{}')".format(customer_data['cid'],customer_data['f_name'],customer_data['l_name']))
         conn.commit()
         ds = {
             'Customer id': customer_data['cid'],
             'First Name': customer_data['f_name'],
             'Last Name': customer_data['l_name'],
-            }
+        }
         print(ds)
         ds = str(ds)
-    except Exception:
-        return json.dumps(ds)
+    except:
+        print("Waiting.....")
+
+
+    return ''
 
 
 def input_data_orders():
     cur = conn.cursor()
     cur.execute("ROLLBACK")
-    cur.execute("select count(*) from dev.advance_jaffle_shop.orders")
+    cur.execute("select count(*) from postgres.advance_jaffle_shop.orders")
     
     length = cur.fetchone()
     m = 0
-    cur.execute('select max(user_id) from dev.advance_jaffle_shop.orders')
+    cur.execute('select max(user_id) from postgres.advance_jaffle_shop.orders')
     m = cur.fetchone()
     print(length)
     customer_order = {}
@@ -76,27 +75,29 @@ def input_data_orders():
 
     customer_order = {}
     customer_order['id'] = index
-    customer_order['user_id'] = randint(1, m[0])
+    customer_order['user_id'] = 100 if m[0]==None else randint(1, m[0])
     customer_order['status'] = random.choice(['completed', 'placed','return_pending','returned','shipped'])
     customer_order['_etl_loaded_at'] = datetime.now()
     customer_order['order_date'] = fake.date_between_dates(date_start=datetime(2015,1,1), date_end=datetime(2019,12,31))
     try:
-        cur.execute("INSERT into dev.advance_jaffle_shop.orders(id, user_id, order_date, status, _etl_loaded_at) values('{}','{}','{}','{}','{}')".format(customer_order['id'],customer_order['user_id'],customer_order['order_date'],customer_order['status'],customer_order['_etl_loaded_at']))
+        cur.execute("INSERT into postgres.advance_jaffle_shop.orders(id, user_id, order_date, status, _etl_loaded_at) values('{}','{}','{}','{}','{}')".format(customer_order['id'],customer_order['user_id'],customer_order['order_date'],customer_order['status'],customer_order['_etl_loaded_at']))
         conn.commit()
-
         ds = {
-            'id': customer_order['id'],
-            'User Id': customer_order['user_id'],
-            'Order Date': customer_order['order_date'],
-            'status': customer_order['status'],
-            '_etl_loaded_at': customer_order['_etl_loaded_at']
+        'id': customer_order['id'],
+        'User Id': customer_order['user_id'],
+        'Order Date': customer_order['order_date'],
+        'status': customer_order['status'],
+        '_etl_loaded_at': customer_order['_etl_loaded_at']
         }
         index = index + 1
         ds = str(ds)
         print(ds)
-    except Exception:
-        index = index + 1
-        return json.dumps(ds)
+    except:
+        print("Waiting.....")
+
+
+
+    return ''
 
 
 
@@ -106,26 +107,26 @@ def generate():
     product_data = {}
 
     product_data = {}
-    cur.execute("select count(*) from dev.advance_stripe.payment")
+    cur.execute("select count(*) from postgres.advance_jaffle_shop.payment")
     length1 = cur.fetchone()
     print(length1)
     index1 = 1 if length1[0] < 1 else length1[0] + 1
 
     m = 0
-    cur.execute('select max(orderid) from dev.advance_stripe.payment')
+    cur.execute('select max(orderid) from postgres.advance_jaffle_shop.payment')
     m = cur.fetchone()
     # print(m,type(m))
 
     payment_data = {}
     payment_data['id'] = index1
-    payment_data['orderid'] = randint(1, m[0])
+    payment_data['orderid'] = 100 if m[0]==None else randint(1, m[0])
     payment_data['paymentmethod'] = random.choice(['bank_transfer', 'coupon','credit_card','gift_card'])
     payment_data['amount'] = fake.pricetag()
     payment_data['amount'] = int(float(fake.pricetag()[1:].replace(',', '')))
     payment_data['_batched_at'] = datetime.now()
     payment_data['created'] = fake.date_between_dates(date_start=datetime(2015,1,1), date_end=datetime(2019,12,31))
-    try :
-        cur.execute("INSERT into dev.advance_stripe.payment(id, orderid, paymentmethod, amount, created, _batched_at) values('{}','{}','{}','{}','{}','{}')".format(payment_data['id'],payment_data['orderid'],payment_data['paymentmethod'],payment_data['amount'],payment_data['created'],payment_data['_batched_at']))
+    try:
+        cur.execute("INSERT into postgres.advance_jaffle_shop.payment(id, orderid, paymentmethod, amount, created, _batched_at) values('{}','{}','{}','{}','{}','{}')".format(payment_data['id'],payment_data['orderid'],payment_data['paymentmethod'],payment_data['amount'],payment_data['created'],payment_data['_batched_at']))
         conn.commit()
 
         ds = {
@@ -139,9 +140,11 @@ def generate():
         index1 = index1 + 1
         ds = str(ds)
         print(ds)
-    except Exception:
-        index1 = index1 + 1
-        return json.dumps({'ds': ds})
+    except:
+        print("Waiting.....")
+
+
+    return ''
 
 
 cur.close()

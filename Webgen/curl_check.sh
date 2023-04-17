825e7db5def59161f36e62cc86546cc8126f2c70
chrisbrown4 <christopher.brown@exeliqconsulting.com>
2023-04-17 15:38:52 -0400

    Curl shell script being added.

---
+++ Webgen/curl_check.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+URL="chrisdgendev.americaniche.com"
+CUSTOMERS="Generate Customers"
+ORDERS="Generate Data Orders"
+PAYMENTS="Generate Paymentsc"
+CUSTOMER_RESULT=$(curl --silent $URL | grep "$CUSTOMERS" | cut -f2 -d ">" | cut -f1 -d "<")
+ORDERS_RESULT=$(curl --silent $URL | grep "$ORDERS" | cut -f2 -d ">" | cut -f1 -d "<")
+PAYMENTS_RESULT=$(curl --silent $URL | grep "$PAYMENTS" | cut -f2 -d ">" | cut -f1 -d "<")
+#if [ "$CUSTOMERS" -eq "$CUSTOMER_RESULT" "$PAYMENTS" -eq "$PAYMENTS_RESULT" && "$ORDERS" -eq "$ORDERS_RESULT" ]
+if [ "$CUSTOMERS" = "$CUSTOMER_RESULT" ] &&  [ "$ORDERS" = "$ORDERS_RESULT" ] && [ "$PAYMENTS" = "$PAYMENTS_RESULT" ]
+then
+    echo " The Customers, Payments and Orders buttons all appear! Merging with QA branch now."
+    git checkout qa
+    git merge development qa
+    echo "Merge successful, pushing branch now."
+    git push origin qa
+    echo "Push successful."
+    
+else
+    echo "One of the buttons was not found."
+fi
+
+
